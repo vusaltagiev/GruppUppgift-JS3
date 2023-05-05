5cdd4f9c123d2428c20ef4e80a439d8362b99f5d
Oz996 <114649736+Oz996@users.noreply.github.com>
2023-05-05 15:10:56 +0200

    details sida

---
+++ Gruppuppgift - eCommerce med React/src/pages/Details.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const productDetails = () => {
+  return (
+    <>
+    <section className='product-details'>
+      <div className="details-images">
+        <div className="display-image">
+          <img src="" alt="" />
+        </div>
+        <div className="thumbnail-div">
+          <img src="" alt="" />
+        </div>
+        <div className="thumbnail-div">
+          <img src="" alt="" />
+        </div>
+        <div className="thumbnail-div">
+          <img src="" alt="" />
+        </div>
+        <div className="thumbnail-div">
+          <img src="" alt="" />
+        </div>
+      </div>
+    </section>
+    </>
+  )
+}
+
+export default productDetails
