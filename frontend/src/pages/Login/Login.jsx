81c540827bc162ae94b0a90f97f2f735f9c1e39f
Marc Carlson <55961315+MarcRichardCarlson@users.noreply.github.com>
2023-05-09 17:18:58 +0200

    Update Login.jsx

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -8,12 +8,12 @@ const Login = () => {
     <Form className='loginForm'> 
         <p className='disclaimer'>Please Login to Your Account</p>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label className='LoginLabel'>E-mail* <p>Don't have a Account yet?</p></Form.Label>
+            <Form.Label className='LoginLabel'>E-mail* <a href="/Register">Don't have a Account yet?</a></Form.Label>
             <Form.Control className='loginInput' type="email"/>
           </Form.Group>
     
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label className='LoginLabel'>Password* <p>Forgot Your Password ?</p></Form.Label>
+            <Form.Label className='LoginLabel'>Password* <a href="#">Forgot Your Password ?</a></Form.Label>
             <Form.Control className='loginInput' type="password"/>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
