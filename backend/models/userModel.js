10fb03fb2dfb58c6c7811c50897352c5c2b41b67
Narek Pogosian <narekpogosian2@gmail.com>
2023-05-15 14:28:04 +0200

    la till login och registering

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,59 +1,56 @@
-const User = require('../schemas/userSchema')
+const User = require("../schemas/userSchema");
 
-const secretKey = process.env.SECRET_KEY
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const secretKey = process.env.SECRET_KEY;
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // Register a new user
 
-exports.registerUser = (req,res) => {
+exports.registerUser = (req, res) => {
+  console.log(req.body);
 
-   console.log(req.body)
+  const { email, password } = req.body;
 
-    const {email, password} = req.body
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const user = new User({ email, password: hashedPassword });
 
-    const hashedPassword = bcrypt.hashSync(password,10)
-    const user = new User({ email, password: hashedPassword})
-
-    user.save()
-     .then(() => {
-
-        const token = jwt.sign({ email: user.email, userId: user._id}, secretKey)
-        res.status(201).json({message: 'User created', token})
-     })
-     .catch((err) => {
-        console.log(err.message)
-        res.status(500).json({message: 'Could not create user'})
-     })
-}
+  user
+    .save()
+    .then(() => {
+      const token = jwt.sign(
+        { email: user.email, userId: user._id },
+        secretKey
+      );
+      res.status(201).json({ message: "User created", token });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res.status(500).json({ message: "Could not create user" });
+    });
+};
 
 // Log in
 
 exports.loginUser = (req, res) => {
+  const { email, password } = req.body;
 
-    const { email, password} = req.body
-
-    User.findOne({email: email})
-     .then((data) => {
-        if(!data || !bcrypt.compareSync(password, data.password)){
-            return res.status(401).json({message: 'Invalid email or password'})
-        }
-        const token = jwt.sign({ email: data.email, dataId: data._id}, secretKey)
-        res.json({message: 'User logged in',token})
-     })
-    
-}
+  User.findOne({ email: email }).then((data) => {
+    if (!data || !bcrypt.compareSync(password, data.password)) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+    const token = jwt.sign({ email: data.email, userId: data._id }, secretKey);
+    res.json({ message: "User logged in", token });
+  });
+};
 
 // Get users
 
-exports.getUsers = (req,res) => {
-
-   User.find()
-    .then(users => {
-      res.status(200).json(users)
+exports.getUsers = (req, res) => {
+  User.find()
+    .then((users) => {
+      res.status(200).json(users);
     })
-    .catch(err => {
-      res.status(500).json({message: 'Could not get users'})
-    })
-
-}
+    .catch((err) => {
+      res.status(500).json({ message: "Could not get users" });
+    });
+};
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,48 +2,53 @@ import { Link, NavLink } from "react-router-dom";
 import "./Header.scss";
 import Logo from "../../images/Logo.svg";
 import { HiShoppingCart } from "react-icons/hi";
-import {BsMoonFill, BsSunFill} from 'react-icons/bs'
-import { useState } from "react";
+import { BsMoonFill, BsSunFill } from "react-icons/bs";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const Header = ({ toggleTheme }) => {
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState("light");
+  const { user, logout } = useContext(AuthContext);
 
   const handleToggle = () => {
-    toggleTheme()
-    setTheme(theme === 'light' ? 'dark' : 'light')
-  }
+    toggleTheme();
+    setTheme(theme === "light" ? "dark" : "light");
+  };
 
   return (
     <div className="header-wrapper">
       <header className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <Link to="/" className="header-logo">
-              <img src={Logo} alt="" />
-            </Link>
-          </div>
-          <div className="col-md-6">
+        <Link to="/" className="header-logo">
+          <img src={Logo} alt="" />
+        </Link>
+
         <div className="header-nav">
           <nav>
-            {
-              theme === 'light' ? (
-
-                <BsMoonFill className="theme-icon" size={19} onClick={handleToggle}/>
-              ): (
-                <BsSunFill className="theme-icon" size={19} onClick={handleToggle}/>
-              )
-            }
+            {theme === "light" ? (
+              <BsMoonFill
+                className="theme-icon"
+                size={19}
+                onClick={handleToggle}
+              />
+            ) : (
+              <BsSunFill
+                className="theme-icon"
+                size={19}
+                onClick={handleToggle}
+              />
+            )}
             <NavLink to="/">Home</NavLink>
             <NavLink to="/products">Products</NavLink>
             <NavLink to="/contact">Contact</NavLink>
-            <NavLink to="/login">Login</NavLink>
+            {!user ? (
+              <NavLink to="/login">Login</NavLink>
+            ) : (
+              <NavLink to="/profile">User Profile</NavLink>
+            )}
           </nav>
-          {/* Shopping Cart */}
           <button className="shopping-cart-button">
             <HiShoppingCart size={25} />
           </button>
-          </div>
-          </div>
         </div>
       </header>
     </div>
