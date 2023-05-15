c0cc5969ab24a3de92b47d4eb68563dfef0f444a
Narek Pogosian <narekpogosian2@gmail.com>
2023-05-15 12:50:01 +0200

    gjorde register sidan och fixade styling på produkter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,27 +7,29 @@ import Products from "./pages/Products/Products";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import Contact from "./pages/Contact/Contact";
-import {ThemeContext} from './contexts/ThemeContext' 
+import { ThemeContext } from "./contexts/ThemeContext";
 import { useState } from "react";
 
 const App = () => {
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState("light");
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? "dark" : 'light')
-  }
+    setTheme(theme === "light" ? "dark" : "light");
+  };
 
   return (
-    <ThemeContext.Provider value={{theme, setTheme}}>
-      <div className={`app ${theme === 'light' ? 'light-theme' : 'dark-theme'}`}>
-        <Header toggleTheme={toggleTheme}/>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div
+        className={`app ${theme === "light" ? "light-theme" : "dark-theme"}`}
+      >
+        <Header toggleTheme={toggleTheme} />
         <main className="app-main">
           <Routes>
             <Route index element={<Home />} />
             <Route path="/products" element={<Products />} />
             <Route path="/details/:productId" element={<Details />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/register" component={<Register />} />
+            <Route path="/register" element={<Register />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
--- frontend/src/pages/Register/Register.jsx
+++ frontend/src/pages/Register/Register.jsx
@@ -1,7 +1,22 @@
 import "./Register.scss";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="register-page">
+      <form className="register-form">
+        <h2>Register</h2>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
