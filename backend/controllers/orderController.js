101a79af7fdfe0a52fecf46d6c98315ece0beaa8
Narek Pogosian <narekpogosian2@gmail.com>
2023-05-19 08:11:08 +0200

    kan se ordrar på profil sidan

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,11 +2,11 @@ const router = require("express").Router();
 const orderModel = require("../models/orderModel");
 const authMiddleware = require("../middleware/authMiddleware");
 
+router.get("/user", authMiddleware, orderModel.getAllOrdersForUser);
+
 router.post("/", authMiddleware, orderModel.createOrder);
 
 router.get("/:id", authMiddleware, orderModel.getAllOrders);
-
-router.get("/user", authMiddleware, orderModel.getAllOrdersForUser);
 
 router.get("/:id", authMiddleware, orderModel.getOrderById);
 
---
+++ frontend/src/components/OrderCard/OrderCard.jsx
@@ -0,0 +1,36 @@
+import "./OrderCard.scss";
+
+const OrderCard = ({ order }) => {
+  return (
+    <li className="order-card" key={order._id}>
+      <div className="flex">
+        <p>
+          <b>Status: </b>
+          <span className="order-status">{order.status}</span>
+        </p>
+        <p>
+          <b>Order Date: </b>
+          <span>{order.createdAt.split("T")[0]}</span>
+        </p>
+      </div>
+      <div>
+        <b>Products</b>
+        <div className="order-products-container">
+          {order.products?.map((p) => (
+            <div className="order-product-card flex" key={p._id}>
+              <div>
+                <img src={p.product.image} alt="" />
+              </div>
+              <div>
+                <b>{p.product.name}</b>
+                <p>Quantity: {p.quantity}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default OrderCard;
