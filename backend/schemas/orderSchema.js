8e39272ba05937ec66f98baf03c736c2c0113bae
Narek Pogosian <narekpogosian2@gmail.com>
2023-05-26 10:21:17 +0200

    uppdaterade order schema, kan nu spara order från kundvagnen

--- backend/schemas/orderSchema.js
+++ backend/schemas/orderSchema.js
@@ -1,14 +1,21 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-
-    user:  { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+const orderSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    status: { type: String, default: "pending" },
     products: [
-        {
-          product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-          quantity: {type: Number, required: true}            
-        }
-      ]
-})
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('Order', orderSchema)
+module.exports = mongoose.model("Order", orderSchema);
--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -1,14 +1,20 @@
+import { useContext, useState } from "react";
 import {
   decrement,
   increment,
   removeItemFromCart,
+  resetCart,
 } from "../../store/cartSlice";
 import "./Cart.scss";
 import { useDispatch, useSelector } from "react-redux";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const Cart = () => {
+  const { user } = useContext(AuthContext);
   const { cartItems } = useSelector((state) => state.cartItems);
   const dispatch = useDispatch();
+
+  const [savedMessage, setSavedMessage] = useState("");
 
   const increaceCartQuantity = (productId) => {
     dispatch(increment(productId));
@@ -20,6 +26,36 @@ const Cart = () => {
 
   const removeFromCart = (productId) => {
     dispatch(removeItemFromCart(productId));
+  };
+
+  const saveOrder = async () => {
+    try {
+      const res = await fetch("http://localhost:7000/orders", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          products: cartItems.map((item) => {
+            return {
+              product: item.productId,
+              quantity: item.quantity,
+            };
+          }),
+        }),
+      });
+
+      if (res.status === 201) {
+        setSavedMessage("Order saved");
+
+        setTimeout(() => {
+          dispatch(resetCart());
+        }, 2000);
+      }
+    } catch (error) {
+      setSavedMessage("Error");
+    }
   };
 
   return (
@@ -40,28 +76,35 @@ const Cart = () => {
                     className="quantityBtn"
                     onClick={() => decreaceCartQuantity(item.productId)}
                   >
-                    {" "}
-                    -{" "}
+                    -
                   </button>
                   <div className="quantityCount">{item.quantity}</div>
                   <button
                     className="quantityBtn"
                     onClick={() => increaceCartQuantity(item.productId)}
                   >
-                    {" "}
-                    +{" "}
+                    +
                   </button>
                   <button
                     className="cartDeleteBtn"
                     onClick={() => removeFromCart(item.productId)}
                   >
-                    {" "}
-                    Ta Bort{" "}
+                    Ta Bort
                   </button>
                 </div>
               </div>
             </div>
           ))}
+          {user ? (
+            <>
+              <button className="save-order" onClick={saveOrder}>
+                Spara Ordern
+              </button>
+              {savedMessage && <p>{savedMessage}</p>}
+            </>
+          ) : (
+            <p>Logga in för att spara</p>
+          )}
         </div>
       )}
     </div>
