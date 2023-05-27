7c0ae044dafa58c8888bebd4e6da1219401e4057
Vusal <vusal@hotmail.com>
2023-05-27 19:43:21 +0200

    färdig med style BestCollectionProduct

--- frontend/src/components/BestCollectionProduct/BestCollectionProduct.jsx
+++ frontend/src/components/BestCollectionProduct/BestCollectionProduct.jsx
@@ -1,5 +1,8 @@
-import './BestCollectionProduct.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './BestCollectionProduct.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'; 
+
 
 
 const BestCollectionProduct = (props) => {
@@ -14,9 +17,9 @@ const BestCollectionProduct = (props) => {
                         <div className="product-details">
                             <h6 className='product-name'><a href="product-details.html">{props.NAME}</a></h6>
                             <p className='product-price'>{props.PRICE}</p>
-                            <a className="product-cart-btn" href="#"><img src="assets/img/icon/cart.png" alt="img" /></a>
+                            <a className="product-cart-btn" href="#"><FontAwesomeIcon icon={faCartPlus} /></a>
                         </div>
-                    </div>
+                    </div>   
             </div>   
         </div>   
     </> 
